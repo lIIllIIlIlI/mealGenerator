56e53d585c4e1bdc2104a49fc37ae4ec164033c2
lIIllIIlIlI <43790987+lIIllIIlIlI@users.noreply.github.com>
2021-10-11 16:34:54 +0200

    Create mealGenerator.py

---
+++ mealGenerator.py
@@ -0,0 +1,76 @@
+import random
+
+meals = {
+    "sandwich": [
+        ["Vollkornbroetchen", "Vollkorntoast", "Dinkelbrot"],
+        ["Mayonnaise", "Senf", "Tomatenmark", "Olivenoel", "BBQ soße", "Frischkäse", "Ziegenkäse"],
+        ["Tomaten", "Gurken", "Ruccola", "Paprika", "Avocado", "Zwiebeln"],
+        ["Putenbrust", "Scheibenkäse", "Schinken", "Thunfisch", "Mozarella", "Gernelen"],
+        ["Salz"],
+        ["Pfeffer"],
+        ["Zitrone"]
+        ],
+    "Smoothie_Gruen": [
+        ["Spinat", "Gurke", "Ruccola", "Avocado", "Basilikum"],
+        ["Apfel", "Kiwi", "Birne", "Blaubeeren"],
+        ["Bannane", "Datteln"],
+        ["Chia Samen", "Makadamia Nüsse", "Wallnüsse", "Ingwer", "Kresse"],
+        ["Süßstoff"],
+        ["Wasser", "Milch", "Kokosmilch"],
+        ["Zitrone"],
+        ["Eiswuerfel"]
+        ],
+    "Smoothie_Rot": [
+        ["Himbeeren", "Blaubeeren", "Beerenmischung", "Orange", "Granatapfel", "Brombeeren", "Erdbeeren"],
+        ["Bannane", "Apfel", "Rote beete", "Karotte", "Pflaume", "Papaya", "Rettich"],
+        ["Chia Samen", "Makadamia Nüsse", "Wallnüsse", "Ingwer", "Minze", "Kokosnuss raspeln", "kurkuma"],
+        ["Süßstoff"],
+        ["Wasser", "Milch", "Kokosmilch"],
+        ["Zitrone"],
+        ["Eiswuerfel"]
+        ],
+    "Bowl": [
+        ["Hähnchen", "Tofu", "Steak", "Fisch", "Entenbrust"],
+        ["Gegrillte Zucchini", "Gegrillte Paprika", "Gegrillte Karotten", "Gebratener Spargel", "Gegrillte Pilze"],
+        ["Vollkorn Nudeln", "Reis", "Süßkartoffeln", "Quinoa"],
+        ["Mango", "Erdbeeren", "Blaubeeren", "Papaya", "Apfel"],
+        ["Tomaten", "Avocado", "Eier", "Garnelen", "Gurke", "Brokkoli", "Radieschen", "Mais", "Kaiserschoten"],
+        ["Koriander", "Basilikum", "Kresse", "Chia Samen", "Sesam", "Erdnüsse"],
+        ["Senfsoße", "Thini soße", "Dairy soße", "Vinaigrette soße", "Pesto soße", "Fruchtige soße"],
+        ],
+    "Salat": [
+        ["Ruccola", "Salatblaetter", "Rettich"],
+        ["Karotten", "Kresse", "Gurke", "Croutons", "Zucchini", "Paprika", "Apfel", "Kresse"],
+        ["Mozarella", "Avocado", "Tomaten", "Reis", "Nudeln", "Oliven"],
+        ["Wassermelone", "Cottage cheese", "Bacon", "Eingelegte Gurken", "Himbeeren", "Orange"],
+        ["Eier", "Thunisch", "Chicken", "Steak", "Tofu"],
+        ["Senfsoße", "Thini soße", "Dairy soße", "Vinaigrette soße", "Pesto soße", "Fruchtige soße"],
+        ],
+    "Risotto": [
+        [],
+    ],
+    "Muesli": [
+        [],
+    ],
+    "Wok": [
+        [],
+    ],
+    "Wraps": [
+        [],
+    ],
+}
+
+generatedMeals = {}
+
+for meal in meals:
+    generatedMeal = []
+    for ingredientType in meals[meal]:
+        if ingredientType:
+            selectedIngredient = random.choice(ingredientType)
+            generatedMeal.append(selectedIngredient)
+    generatedMeals.update({meal: generatedMeal})
+
+print("\n\n             *** Generated meals ***")
+for name, ingredients in generatedMeals.items():
+    print("{} : {}".format(name, ingredients))
+print("\n\n")
